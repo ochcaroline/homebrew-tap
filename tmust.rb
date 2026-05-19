e6b7a4596388288d45081d53626ab1ddb0b9bcfc
ochcaroline <28461795+ochcaroline@users.noreply.github.com>
2026-05-19 16:01:13 +0200

    chore: update tmust

--- tmust.rb
+++ tmust.rb
@@ -1,9 +1,9 @@
 class Tmust < Formula
   desc "A minimal tmux session manager that integrates with zoxide."
   homepage "https://github.com/ochcaroline/tmust"
-  url "https://github.com/ochcaroline/tmust.git", tag: "v0.3.0"
+  url "https://github.com/ochcaroline/tmust.git", tag: "v0.3.1"
   
-  version "0.3.0"
+  version "0.3.1"
 
   license "YOLO"
 
