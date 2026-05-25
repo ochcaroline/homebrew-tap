7314d302091c41187ffbf0e7e56b48352aeaaf60
ochcaroline <28461795+ochcaroline@users.noreply.github.com>
2026-05-25 19:36:01 +0200

    chore: update k1s

--- k1s.rb
+++ k1s.rb
@@ -1,9 +1,9 @@
 class K1s < Formula
   desc "K1s, kubernetes made simple"
   homepage "https://github.com/ochcaroline/k1s"
-  url "https://github.com/ochcaroline/k1s.git", tag: "v0.0.3"
+  url "https://github.com/ochcaroline/k1s.git", tag: "v0.0.4"
   
-  version "0.0.3"
+  version "0.0.4"
 
   license "YOLO"
 
